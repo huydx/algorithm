e7270a43b7cf4598f4c13e98ebe7eafa99fbff72
huydx <xuanhuy.do@linecorp.com>
2020-03-08 10:08:51 +0900

    (add) accepted 10226

--- src/uva/Task10226.java
+++ src/uva/Task10226.java
@@ -8,31 +8,24 @@ import java.util.TreeMap;
 
 public class Task10226 {
     public void solve(int testNumber, Scanner in, PrintWriter out) {
-        int T = in.nextInt();
+        int T = Integer.parseInt(in.nextLine());
         in.nextLine();
-        int c = 0;
-        for (int j = 0; j < T; j++) {
+        for (int i = 0; i < T; i++) {
             Map<String, Integer> map = new TreeMap<>();
-            while (true) {
+            double c = 0;
+            while (in.hasNextLine()) {
                 String s = in.nextLine();
-                if (s == null || s.equals("")) {
+                if (s == null || s.isEmpty() || s.trim().isEmpty()) {
                     break;
-                } else {
-                    c++;
-                    Integer i = map.get(s);
-                    if (i == null) {
-                        i = 0;
-                    }
-                    map.put(s, i + 1);
                 }
+                map.merge(s, 1, Integer::sum);
+                c++;
             }
             for (Entry<String, Integer> e : map.entrySet()) {
-                out.println(String.format("%s %.4f", e.getKey(), (float) 100 * e.getValue() / c));
+                out.append(String.format("%s %.4f\n", e.getKey(), 100 * e.getValue() / c));
             }
-            if (j == T-1) {
-                return;
-            } else {
-                in.nextLine();
+            if (i < T - 1) {
+                out.println("");
             }
         }
     }
